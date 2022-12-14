98e74b7702ac19e516627818a66a6b0ee0eeabdc
Mustafa <mustafausama@outlook.com>
2022-12-15 01:49:58 +0200

    backend node-ts eslint

---
+++ backend/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-console": 1,
+    "no-var": "error",
+    "prefer-const": "error"
+  }
+};
